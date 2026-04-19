ec0226a10c4c6fd9a34cdfd2d129526ce04755e6
Tanay Sharma <tanaysharma3019@gmail.com>
2026-04-19 21:09:26 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -5,4 +5,5 @@
 const SUPABASE_URL = "https://iqgqpwbfnrbmtvbpgrlv.supabase.co";   // ← REPLACE
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlxZ3Fwd2JmbnJibXR2YnBncmx2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2MDI1NDgsImV4cCI6MjA5MjE3ODU0OH0.A0fSGwitk2aSPDwUzsarOqnI1x9t6vTbt4cePAnhhQQ";              // ← REPLACE
                       
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window._sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
